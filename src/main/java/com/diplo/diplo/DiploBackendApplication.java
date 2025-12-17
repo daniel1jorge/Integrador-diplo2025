590b00ff83d9703e62671c1a3bf4ddbfa85ece38
Jorge Daniel Silva <daniel1jorge@hotmail.com>
2025-12-16 23:32:10 -0300

    add files proyecto inicial.

---
+++ src/main/java/com/diplo/diplo/DiploBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diplo.diplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiploBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiploBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplo/diplo/DiploBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplo.diplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiploBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
